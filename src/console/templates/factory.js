7be827c120b82778ec34a26f9829b783b952da63
isam chajia <isamchajiabeat@gmail.com>
2025-08-28 17:20:11 +0100

    done

--- src/console/templates/factory.js
+++ src/console/templates/factory.js
@@ -1,9 +1,27 @@
-import { faker } from "@faker-js/faker";
+import faker from "faker"; 
+import __NAME__ from "../models/__NAME__.js";
 
-export const make__NAME__ = () => {
-  return {
-    name: faker.person.fullName(),
+// Faker Examples:
+// faker.name.firstName();   // الاسم الأول
+// faker.name.lastName();    // الاسم الأخير
+// faker.name.fullName();    // الاسم الكامل
+// faker.name.jobTitle();    // المسمى الوظيفي
+// faker.name.prefix();      // مثل Mr., Mrs.
+// faker.name.suffix();      // مثل Jr., Sr.
+// faker.internet.email();       // بريد إلكتروني
+// faker.internet.userName();    // اسم مستخدم
+// faker.internet.password();    // كلمة مرور
+// faker.internet.url();         // رابط موقع
+// faker.internet.avatar();      // رابط صورة شخصية
+
+export const createFake__NAME__ = async () => {
+  const item = new __NAME__({
+
+    name: faker.name.fullName(),
     email: faker.internet.email(),
-    createdAt: faker.date.past(),
-  };
+
+  });
+
+  await item.save();
+  return item;
 };
